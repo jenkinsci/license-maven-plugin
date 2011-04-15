872d1147130dd9b5c330cb710a26b52a12b00491
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-04-15 09:34:07 -0700

    cache complied templates

--- src/main/resources/com/cloudbees/maven/license/htmlgen.groovy
+++ src/main/resources/com/cloudbees/maven/license/htmlgen.groovy
@@ -1,10 +1,15 @@
+// generate HTML report from XML
 import javax.xml.transform.TransformerFactory
 import javax.xml.transform.stream.StreamSource
 import javax.xml.transform.stream.StreamResult
+import javax.xml.transform.Templates
 
 mojo.generateLicenseHtml.parentFile?.mkdirs()
 
-// generate HTML report from XML
-def t = TransformerFactory.newInstance().newTemplates(new StreamSource(mojo.class.getResource("licenses.xslt").toExternalForm()));
+private Templates t;
+
+if (t==null)
+    t = TransformerFactory.newInstance().newTemplates(new StreamSource(mojo.class.getResource("licenses.xslt").toExternalForm()));
+
 t.newTransformer().transform(new StreamSource(mojo.generateLicenseXml), new StreamResult(mojo.generateLicenseHtml));
 log.info("Generated ${mojo.generateLicenseHtml}")
