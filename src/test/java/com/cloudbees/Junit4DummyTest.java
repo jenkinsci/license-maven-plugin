c2e683f1d7a67f8d45b9a54ed1d0576ce04cfe86
Alexander Brandes <mc.cache@web.de>
2023-07-04 18:01:28 +0200

    Misc. cleanup

---
+++ src/test/java/com/cloudbees/Junit4DummyTest.java
@@ -0,0 +1,14 @@
+package org.jvnet.maven.jellydoc;
+
+import org.junit.Test;
+
+public class Junit4DummyTest {
+
+    @Test
+    public void anything() {
+        /*
+         * Intentionally blank. We just want a test that runs with JUnit so that buildPlugin() works
+         * in the Jenkinsfile.
+         */
+    }
+}
